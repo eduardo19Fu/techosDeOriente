05ca2a8a9fbbafe0199d1b1540fffbd9684a96c0
Eduardo Fuentes <ramieduar@gmail.com>
2022-04-27 14:01:26 -0600

    Se agrego url de origen del backend en heroku

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -18,9 +18,9 @@ export class AuthService {
   constructor(
     private http: HttpClient
   ) {
-    this.url = 'http://localhost:8282';
-    // this.url = 'https://dtodojalapa.herokuapp.com';
-    // this.url = 'http://31.220.56.29:8282';
+    // this.url = 'http://localhost:8282';
+    this.url = 'https://dtodojalapa.herokuapp.com';
+    // this.url = 'https://31.220.56.29:8282';
   }
 
   public get usuario(): Usuario{
