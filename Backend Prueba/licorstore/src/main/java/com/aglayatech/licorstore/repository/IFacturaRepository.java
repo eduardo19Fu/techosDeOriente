5564c2c2d7a0bcc50ff9a0149ef2bbfe218727c8
Eduardo Fuentes <ramieduar@gmail.com>
2022-07-07 09:14:04 -0600

    Se elimino carpeta Backend destuck

--- Backend Prueba/licorstore/src/main/java/com/aglayatech/licorstore/repository/IFacturaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aglayatech.licorstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.aglayatech.licorstore.model.Factura;
-
-public interface IFacturaRepository extends JpaRepository<Factura, Long> {
-}
--- Backend Prueba/licorstore/src/main/java/com/aglayatech/licorstore/repository/IProformaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aglayatech.licorstore.repository;
-
-import com.aglayatech.licorstore.model.Proforma;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProformaRepository extends JpaRepository<Proforma, Long> {
-}
