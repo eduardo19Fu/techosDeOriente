30f6fb554a00e371a2995e4c66515961b503ccfc
Eduardo Fuentes <ramieduar@gmail.com>
2022-09-10 14:28:42 -0600

    Se completaron los cambios para la impresion del reporte donde se plasma el envio a realizarse, ademas de eliminar archivos innecesarios del proyecto.

--- Frontend/src/app/components/envios/envios.component.ts
+++ Frontend/src/app/components/envios/envios.component.ts
@@ -101,4 +101,22 @@ export class EnviosComponent implements OnInit {
     this.detailService.abrirModal();
   }
 
+  print(envio: Envio): void {
+    this.envioService.getEnvioPdf(envio.idEnvio).subscribe(response => {
+      const url = window.URL.createObjectURL(response.data);
+      const a = document.createElement('a');
+      document.body.appendChild(a);
+      a.setAttribute('style', 'display: none');
+      a.setAttribute('target', 'blank');
+      a.href = url;
+
+      window.open(a.toString(), '_blank');
+      window.URL.revokeObjectURL(url);
+      a.remove();
+    },
+      error => {
+        console.log(error);
+    });
+  }
+
 }
